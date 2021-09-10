c50a48feb80c976d6cee45df4b3060dac8317111
sachintha00 <sachintha0603@gmail.com>
2021-09-10 21:23:14 +0530

    updated

--- Back-end/CourseManagementSystem/src/main/java/com/cms/demo/controller/CMScontroller.java
+++ Back-end/CourseManagementSystem/src/main/java/com/cms/demo/controller/CMScontroller.java
@@ -1,13 +1,22 @@
 package com.cms.demo.controller;
 
+import java.awt.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cms.demo.model.CMSmodel;
+
 @RestController
 public class CMScontroller {
-
+	
 	@GetMapping("/home")
 	public String home() {
 		return "this is the home page";
+	}
+	
+	@GetMapping("/cources")
+	public java.util.List<CMSmodel> getCources() {
+		
 	}
 }
---
+++ Back-end/CourseManagementSystem/src/main/java/com/cms/demo/service/CMSservice.java
@@ -0,0 +1,5 @@
+package com.cms.demo.service;
+
+public class CMSservice {
+
+}
