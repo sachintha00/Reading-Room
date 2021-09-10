89cff0f97d491cc5d169c57d3f33daa7c136b3b5
sachintha00 <sachintha0603@gmail.com>
2021-09-10 20:46:33 +0530

    updated

---
+++ Back-end/CourseManagementSystem/src/test/java/com/cms/demo/CourseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cms.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
