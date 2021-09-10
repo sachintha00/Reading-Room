30a4326ea75f5e0529961672ca2146fa19471846
sachintha00 <sachintha0603@gmail.com>
2021-09-10 21:12:46 +0530

    updated

--- Back-end/CourseManagementSystem/src/main/java/com/cms/demo/model/CMSmodel.java
+++ Back-end/CourseManagementSystem/src/main/java/com/cms/demo/model/CMSmodel.java
@@ -5,4 +5,42 @@ public class CMSmodel {
 	private long coursId;
 	private String title;
 	private String description;
+	
+	public CMSmodel(long coursId, String title, String description) {
+		super();
+		this.coursId = coursId;
+		this.title = title;
+		this.description = description;
+	}
+
+	public CMSmodel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public long getCoursId() {
+		return coursId;
+	}
+
+	public void setCoursId(long coursId) {
+		this.coursId = coursId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
 }
