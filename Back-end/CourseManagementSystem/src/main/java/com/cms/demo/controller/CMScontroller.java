21cc89502a01aca7b57eadb69a2aab932d2aae14
sachintha00 <sachintha0603@gmail.com>
2021-09-11 11:44:12 +0530

    updated

--- Back-end/CourseManagementSystem/src/main/java/com/cms/demo/controller/CMScontroller.java
+++ Back-end/CourseManagementSystem/src/main/java/com/cms/demo/controller/CMScontroller.java
@@ -2,13 +2,18 @@ package com.cms.demo.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cms.demo.model.CMSmodel;
+import com.cms.demo.service.CMSservice;
 
 @RestController
 public class CMScontroller {
+	
+	@Autowired
+	private CMSservice cmSservice;
 	
 	@GetMapping("/home")
 	public String home() {
@@ -17,6 +22,6 @@ public class CMScontroller {
 	
 	@GetMapping("/cources")
 	public List<CMSmodel> getCources() {
-		return null;
+		return this.cmSservice.getCources();
 	}
 }
