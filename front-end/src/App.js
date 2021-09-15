41e3227dcafcb9c41c90de095762f70f46386418
sachintha00 <sachintha0603@gmail.com>
2021-09-15 19:57:39 +0530

    updated

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,10 +5,16 @@ import base_url from "./Api/BootApi";
 import { useState } from "react";
 
 function App() {
+	var err = "";
 	const [newCourse, courseMethod] = useState([]);
+	const [tempcourse, setTempCourse] = useState([]);
 	const handleForm = (e) => {
-		console.log(newCourse);
-		postDataSend(newCourse);
+		// console.log(newCourse);
+		// postDataSend(newCourse);
+		const temp = getCourse();
+		tempcourse.map((data) => {
+			return data.title === temp.title ? (err = "fdgsdgfdsgfsdg") : (err = "");
+		});
 		e.preventDefault();
 	};
 
@@ -16,7 +22,7 @@ function App() {
 		axios.get(`${base_url}/courses`).then(
 			(Response) => {
 				console.log(Response.data);
-				courseMethod(newCourse.concat(Response.data));
+				return courseMethod(Response.data);
 			},
 			(error) => {
 				console.log(error);
@@ -24,23 +30,23 @@ function App() {
 		);
 	};
 
-	const postDataSend = (newCourse) => {
-		axios.post(`${base_url}/courses`, newCourse).then(
-			(Response) => {
-				console.log(Response);
-				console.log("Success");
-			},
-			(error) => {
-				console.log(error);
-				console.log("error");
-			}
-		);
-	};
+	// const postDataSend = (newCourse) => {
+	// 	axios.post(`${base_url}/courses`, newCourse).then(
+	// 		(Response) => {
+	// 			console.log(Response);
+	// 			console.log("Success");
+	// 		},
+	// 		(error) => {
+	// 			console.log(error);
+	// 			console.log("error");
+	// 		}
+	// 	);
+	// };
 
 	return (
 		<div className="App">
 			<button onClick={getCourse}>Load Data</button>
-
+			<h1>{err}</h1>
 			<div>
 				{newCourse.length > 0
 					? newCourse.map((info) => {
@@ -56,7 +62,7 @@ function App() {
 					id=""
 					placeholder="Course ID"
 					onChange={(e) => {
-						courseMethod({ ...newCourse, coursId: e.target.value });
+						courseMethod({ ...setTempCourse, coursId: e.target.value });
 					}}
 				/>
 				<input
@@ -65,7 +71,7 @@ function App() {
 					id=""
 					placeholder="Course Title"
 					onChange={(e) => {
-						courseMethod({ ...newCourse, title: e.target.value });
+						courseMethod({ ...setTempCourse, title: e.target.value });
 					}}
 				/>
 				<input
@@ -74,7 +80,7 @@ function App() {
 					id=""
 					placeholder="Course Description"
 					onChange={(e) => {
-						courseMethod({ ...newCourse, description: e.target.value });
+						courseMethod({ ...setTempCourse, description: e.target.value });
 					}}
 				/>
 
