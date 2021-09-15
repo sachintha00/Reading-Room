363d4c6e8428a82f8be25926c7d18deb71fad221
sachintha00 <sachintha0603@gmail.com>
2021-09-15 13:43:52 +0530

    updated

--- Back-end/CourseManagementSystem/src/main/java/com/cms/demo/service/CMSserviceImple.java
+++ Back-end/CourseManagementSystem/src/main/java/com/cms/demo/service/CMSserviceImple.java
@@ -5,15 +5,20 @@ import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.cms.demo.dao.CourseDao;
 import com.cms.demo.model.CMSmodel;
 
 @Service
 public class CMSserviceImple implements CMSservice {
 
+	@Autowired
+	private CourseDao courseDao;
+	
 	List<CMSmodel> list;
 	
 	public CMSserviceImple() {
