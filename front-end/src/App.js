f62608c47ab6b05a1b38fbf2a95ed8c41cd36e26
sachintha00 <sachintha0603@gmail.com>
2021-09-15 15:27:57 +0530

    updated

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 
 import axios from "axios";
 import base_url from "./Api/BootApi";
+import { useState } from "react";
 
 function App() {
 	const getCourse = () => {
@@ -14,9 +15,51 @@ function App() {
 			}
 		);
 	};
+
+	const postDataSend=(newCourse)=>{
+		axios.post(`${base_url}/courses`)
+	}
+
+	const [newCourse, courseMethod] = useState({});
+	const handleForm = (e) => {
+		console.log(newCourse);
+		e.preventDefault();
+	};
 	return (
 		<div className="App">
 			<button onClick={getCourse}>Load Data</button>
+
+			<form onSubmit={handleForm}>
+				<input
+					type="text"
+					name="cId"
+					id=""
+					placeholder="Course ID"
+					onChange={(e) => {
+						courseMethod({ ...newCourse, id: e.target.value });
+					}}
+				/>
+				<input
+					type="text"
+					name="cTitel"
+					id=""
+					placeholder="Course Title"
+					onChange={(e) => {
+						courseMethod({ ...newCourse, title: e.target.value });
+					}}
+				/>
+				<input
+					type="text"
+					name="cDescription"
+					id=""
+					placeholder="Course Description"
+					onChange={(e) => {
+						courseMethod({ ...newCourse, description: e.target.value });
+					}}
+				/>
+
+				<button type="submit">Add Course</button>
+			</form>
 		</div>
 	);
 }
