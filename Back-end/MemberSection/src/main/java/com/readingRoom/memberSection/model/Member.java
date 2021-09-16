534f656c47f3321e06ab29f936cef9644f76f882
sachintha00 <sachintha0603@gmail.com>
2021-09-16 10:48:24 +0530

    updated

--- Back-end/MemberSection/src/main/java/com/readingRoom/memberSection/model/Member.java
+++ Back-end/MemberSection/src/main/java/com/readingRoom/memberSection/model/Member.java
@@ -1,12 +1,15 @@
 package com.readingRoom.memberSection.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Member {
 	
-	@Id
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long memberId;
 	private String memberName;
 	private String memberNic;
