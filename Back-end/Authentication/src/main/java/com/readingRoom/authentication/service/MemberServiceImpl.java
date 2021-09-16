4413024ec81f094df78f4427fade9067898890c2
sachintha00 <sachintha0603@gmail.com>
2021-09-16 09:27:29 +0530

    updated

--- Back-end/Authentication/src/main/java/com/readingRoom/authentication/service/MemberServiceImpl.java
+++ Back-end/Authentication/src/main/java/com/readingRoom/authentication/service/MemberServiceImpl.java
@@ -2,14 +2,20 @@ package com.readingRoom.authentication.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.readingRoom.authentication.model.Member;
+import com.readingRoom.authentication.repository.MemberRepository;
 
 public class MemberServiceImpl implements MemberService {
 
+	
+	@Autowired
+	private MemberRepository memberRepository;
+	
 	@Override
 	public List<Member> getAllMembers() {
-		// TODO Auto-generated method stub
-		return null;
+		return memberRepository.findAll();
 	}
 
 	@Override
