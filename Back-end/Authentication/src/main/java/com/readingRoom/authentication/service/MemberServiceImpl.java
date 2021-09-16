3520e87d020eced0eaea1c8fefe423365222fb6e
sachintha00 <sachintha0603@gmail.com>
2021-09-16 09:24:45 +0530

    updated

---
+++ Back-end/Authentication/src/main/java/com/readingRoom/authentication/service/MemberServiceImpl.java
@@ -0,0 +1,39 @@
+package com.readingRoom.authentication.service;
+
+import java.util.List;
+
+import com.readingRoom.authentication.model.Member;
+
+public class MemberServiceImpl implements MemberService {
+
+	@Override
+	public List<Member> getAllMembers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Member getCourse(long memberId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Member addMember(Member member) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Member updatedMember(Member member) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeMember(long memberId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
