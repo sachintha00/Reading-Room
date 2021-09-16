6ca286a9e0ada2cdb3e3ff09fc2e2123bd06c36b
sachintha00 <sachintha0603@gmail.com>
2021-09-16 12:03:04 +0530

    updated

---
+++ Back-end/BookService/src/main/java/com/readingRoom/bookServie/model/Book.java
@@ -0,0 +1,10 @@
+package com.readingRoom.bookServie.model;
+
+public class Book {
+	private long bookId;
+	private String ISBN;
+	private String bookName;
+	private String bookType;
+	private String medium;
+	private String AuthorName;
+}
--- Back-end/CourseManagementSystem/src/main/java/com/cms/demo/dao/CourseDao.java
+++
@@ -1,9 +0,0 @@
-package com.cms.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cms.demo.model.CMSmodel;
-
-public interface CourseDao extends JpaRepository<CMSmodel, Long>{
-
-}
