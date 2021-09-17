70796dc8b1516fc8f128b9a50b6acb6537b49f19
sachintha00 <sachintha0603@gmail.com>
2021-09-17 11:55:25 +0530

    updated

---
+++ Back-end/BookService/src/main/java/com/readingRoom/bookServie/service/SupplierService.java
@@ -0,0 +1,16 @@
+package com.readingRoom.bookServie.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.readingRoom.bookServie.model.Supplier;
+
+public interface SupplierService {
+
+	public List<Supplier> getAllSupplier();
+	public Optional<Supplier> getSupplier(long supplierId);
+	public Optional<Supplier> getSupplierByName(long name);
+	public Supplier addSupplier(Supplier supplier);
+	public Supplier updateSupplier(Supplier supplier);
+	public void removeSupplier(long supplierId);
+}
---
+++ Back-end/BookService/src/main/java/com/readingRoom/bookServie/service/SupplierServiceImpl.java
@@ -0,0 +1,46 @@
+package com.readingRoom.bookServie.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.readingRoom.bookServie.model.Supplier;
+
+public class SupplierServiceImpl implements SupplierService{
+
+	@Override
+	public List<Supplier> getAllSupplier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Supplier> getSupplier(long supplierId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Supplier> getSupplierByName(long name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Supplier addSupplier(Supplier supplier) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Supplier updateSupplier(Supplier supplier) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeSupplier(long supplierId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
