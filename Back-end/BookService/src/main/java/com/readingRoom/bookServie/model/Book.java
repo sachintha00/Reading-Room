f29be6ad23295c820278dad97eadfc4cbb91e5f4
sachintha00 <sachintha0603@gmail.com>
2021-09-19 20:43:29 +0530

    updated

--- Back-end/BookService/src/main/java/com/readingRoom/bookServie/model/Book.java
+++ Back-end/BookService/src/main/java/com/readingRoom/bookServie/model/Book.java
@@ -1,16 +1,9 @@
 package com.readingRoom.bookServie.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 @Entity
 public class Book {
 	
--- Back-end/BookService/src/main/java/com/readingRoom/bookServie/model/Supplier.java
+++ Back-end/BookService/src/main/java/com/readingRoom/bookServie/model/Supplier.java
@@ -1,12 +1,10 @@
 package com.readingRoom.bookServie.model;
 
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Supplier {
