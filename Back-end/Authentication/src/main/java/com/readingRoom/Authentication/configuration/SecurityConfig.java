0f673a70665858ba90ff8a601d5472c4ecf9862b
sachintha00 <sachintha0603@gmail.com>
2021-09-20 14:11:46 +0530

    updated

--- Back-end/Authentication/src/main/java/com/readingRoom/Authentication/configuration/SecurityConfig.java
+++ Back-end/Authentication/src/main/java/com/readingRoom/Authentication/configuration/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.readingRoom.Authentication.configuration;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 }
