2b3028cf321832913508370fc1f2692d5af05611
sachintha00 <sachintha0603@gmail.com>
2021-09-20 14:11:12 +0530

    updated

---
+++ Back-end/Authentication/src/main/java/com/readingRoom/Authentication/configuration/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.readingRoom.Authentication.configuration;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+}
