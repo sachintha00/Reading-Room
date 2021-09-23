30c0ebae830901af577e00289c9d61b7a5d56615
sachintha00 <sachintha0603@gmail.com>
2021-09-23 11:38:09 +0530

    updated

--- Front End/src/index.js
+++ Front End/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-// import { Provider } from "react-redux";
-// import store from "./redux/store";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<Provider store={store}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Provider>,
 	document.getElementById("root")
 );
