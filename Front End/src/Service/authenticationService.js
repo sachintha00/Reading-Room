d272458d10237d872c84e2256ee7c6a4931bd135
sachintha00 <sachintha0603@gmail.com>
2021-09-23 11:39:31 +0530

    Update authenticationService.js

--- Front End/src/Service/authenticationService.js
+++ Front End/src/Service/authenticationService.js
@@ -8,7 +8,7 @@ const getToken = () => {
 export const userLogin = (authRequest) => {
 	return axios({
 		method: "POST",
-		url: "http://localhost:8081/api/v1/auth/login",
+		url: "http://localhost:8083/api/v1/auth/login",
 		data: authRequest,
 	});
 };
@@ -17,7 +17,7 @@ export const fetchUserData = (authRequest) => {
 	return axios({
 		method: "GET",
 		url: `${
-			process.env.hostUrl || "http://localhost:8081"
+			process.env.hostUrl || "http://localhost:8083"
 		}/api/v1/auth/userinfo`,
 		headers: {
 			Authorization: "Bearer " + getToken(),
