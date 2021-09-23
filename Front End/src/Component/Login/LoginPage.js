0f408f0436ac90767223079aad8ef0b2b37ae513
sachintha00 <sachintha0603@gmail.com>
2021-09-23 13:58:44 +0530

    updated

--- Front End/src/Component/Login/LoginPage.js
+++ Front End/src/Component/Login/LoginPage.js
@@ -61,93 +61,119 @@ const LoginPage = ({ loading, error, ...props }) => {
 		<>
 			<section className="sign-in">
 				<div className="container">
-					<div className="signin-content">
-						<div className="signin-image">
-							<figure>
-								<img src={LoginImage} alt="sing up image" />
-							</figure>
-						</div>
+					<div
+						className="sec-container"
+						style={{ position: "relative", top: "-20px" }}
+					>
+						<div className="signin-content">
+							<div className="signin-image">
+								<figure>
+									<img src={LoginImage} alt="sing up image" />
+								</figure>
+							</div>
 
-						<div className="signin-form">
-							<h2 className="form-title">
-								<span style={{ fontSize: "1.5rem", paddingBottom: "30px" }}>
-									Sing In
-								</span>
-								<br />
-								<span
-									style={{
-										color: "red",
-										fontWeight: "bold",
-									}}
-								>
-									Reading
-								</span>
-								<span>Room</span>
-							</h2>
-							{error && (
-								<ErrorMessage>
-									{error}
-									Incorrect username or password !!
-								</ErrorMessage>
-							)}
-							<form
-								onSubmit={handleSubmit}
-								noValidate={false}
-								className="register-form"
-								id="login-form"
-							>
-								<div className="form-group">
-									<label for="your_name">
-										<i className="zmdi zmdi-account material-icons-name"></i>
-									</label>
-									<input
-										id="username"
-										type="text"
-										minLength={5}
-										value={values.userName}
-										onChange={handleChange}
-										name="userName"
-										placeholder="Username"
-									/>
-								</div>
-								<div className="form-group">
-									<label for="your_pass">
-										<i className="zmdi zmdi-lock"></i>
-									</label>
-									<input
-										id="password"
-										type="password"
-										minLength={8}
-										value={values.password}
-										onChange={handleChange}
-										name="password"
-										placeholder="Password"
-									/>
-								</div>
-								<div className="form-group">
-									<input
-										type="checkbox"
-										name="remember-me"
-										id="remember-me"
-										className="agree-term"
-									/>
-									<label for="remember-me" className="label-agree-term">
-										<span>
-											<span></span>
+							<div className="signin-form">
+								<h2 className="form-title">
+									<div style={{ position: "relative", marginBottom: "20px" }}>
+										<span
+											style={{
+												color: "#025B9A",
+												fontWeight: "bold",
+												fontFamily: "sans-serif",
+											}}
+										>
+											Reading
 										</span>
-										Remember me
-									</label>
-								</div>
-								<div className="form-group form-button">
-									<input
-										type="submit"
-										name="signin"
-										id="signin"
-										class="form-submit"
-										value="Log in"
-									/>
-								</div>
-							</form>
+										<span
+											style={{
+												color: "#216CC8",
+												fontWeight: "bold",
+												fontFamily: "sans-serif",
+											}}
+										>
+											Room
+										</span>
+									</div>
+									<p
+										style={{
+											color: "#5F6D8C",
+											fontFamily: "sans-serif",
+											width: "500px",
+											marginBottom: "40px",
+										}}
+									>
+										Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+										Impedit consectetur nihil blanditiis nulla qui, similique
+									</p>
+									<span style={{ fontSize: "1.5rem", paddingBottom: "30px" }}>
+										Sing In
+									</span>
+								</h2>
+								{error && (
+									<ErrorMessage>
+										{error}
+										Incorrect username or password !!
+									</ErrorMessage>
+								)}
+								<form
+									onSubmit={handleSubmit}
+									noValidate={false}
+									className="register-form"
+									id="login-form"
+								>
+									<div className="form-group">
+										<label for="your_name">
+											<i className="zmdi zmdi-account material-icons-name"></i>
+										</label>
+										<input
+											id="username"
+											type="text"
+											minLength={5}
+											value={values.userName}
+											onChange={handleChange}
+											name="userName"
+											placeholder="Username"
+										/>
+									</div>
+									<div className="form-group">
+										<label for="your_pass">
+											<i className="zmdi zmdi-lock"></i>
+										</label>
+										<input
+											id="password"
+											type="password"
+											minLength={8}
+											value={values.password}
+											onChange={handleChange}
+											name="password"
+											placeholder="Password"
+										/>
+									</div>
+									<div className="form-group">
+										<input
+											type="checkbox"
+											name="remember-me"
+											id="remember-me"
+											className="agree-term"
+										/>
+										<label for="remember-me" className="label-agree-term">
+											<span>
+												<span></span>
+											</span>
+											Remember me
+										</label>
+									</div>
+									<div className="form-group form-button">
+										<input
+											type="submit"
+											name="signin"
+											id="signin"
+											class="form-submit"
+											value="Log in"
+										/>
+									</div>
+								</form>
+							</div>
 						</div>
 					</div>
 				</div>
