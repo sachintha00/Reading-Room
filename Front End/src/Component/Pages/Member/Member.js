c7639186442b5a19a0357fa396c20d6278e6344b
sachintha00 <sachintha0603@gmail.com>
2021-09-24 14:50:33 +0530

    updated

--- Front End/src/Component/Pages/Member/Member.js
+++ Front End/src/Component/Pages/Member/Member.js
@@ -77,24 +77,9 @@ export default class componentName extends Component {
 							</tr>
 						</thead>
 						<tbody>
-							<tr>
-								<th scope="row">1</th>
-								<td>Mark</td>
-								<td>Otto</td>
-								<td>@mdo</td>
-							</tr>
-							<tr>
-								<th scope="row">2</th>
-								<td>Jacob</td>
-								<td>Thornton</td>
-								<td>@fat</td>
-							</tr>
-							<tr>
-								<th scope="row">3</th>
-								<td>Larry</td>
-								<td>the Bird</td>
-								<td>@twitter</td>
-							</tr>
+							{memb.map(memb=>
+                                <tr>key = {memb.}</tr>
+                                )}
 						</tbody>
 					</table>
 				</TableSection>
