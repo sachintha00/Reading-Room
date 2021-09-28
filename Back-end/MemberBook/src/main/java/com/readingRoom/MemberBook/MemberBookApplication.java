2a58b7f98f493ef47090cfd894d8e2767f7b83da
sachintha00 <sachintha0603@gmail.com>
2021-09-28 17:23:10 +0530

    updated

---
+++ Back-end/MemberBook/src/main/java/com/readingRoom/MemberBook/MemberBookApplication.java
@@ -0,0 +1,13 @@
+package com.readingRoom.MemberBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberBookApplication.class, args);
+	}
+
+}
---
+++ Back-end/MemberBook/src/test/java/com/readingRoom/MemberBook/MemberBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readingRoom.MemberBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
