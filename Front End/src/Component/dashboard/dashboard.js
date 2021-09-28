6a26ca0089eac2ca0644124328662685c8dd365e
sachintha00 <sachintha0603@gmail.com>
2021-09-28 08:49:22 +0530

    updated

--- Front End/src/Component/dashboard/dashboard.js
+++ Front End/src/Component/dashboard/dashboard.js
@@ -9,6 +9,7 @@ import Book from "../Pages/Book/Book";
 import Member from "../Pages/Member/Member";
 import Suppliers from "../Pages/SupplierPage/Suppliers";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import "./DashBoardStyle.css";
 
 const MainWrapper = styled.div`
 	padding-top: 40px;
@@ -54,7 +55,7 @@ const Dashboard = (props) => {
 		<>
 			<BrowserRouter>
 				<SideBarSec>
-					<List>
+					{/* <List>
 						<ListItem>
 							<Link to="/dashboard">Home</Link>
 						</ListItem>
@@ -67,7 +68,263 @@ const Dashboard = (props) => {
 					</List>
 					<Button style={{ marginTop: "5px" }} onClick={() => logOut()}>
 						Logout
-					</Button>
+					</Button> */}
+
+					{/* <nav class="main-menu">
+						<ul>
+							<li>
+								<a href="http://justinfarrow.com">
+									<i class="fa fa-home fa-2x"></i>
+									<span class="nav-text">Dashboard</span>
+								</a>
+							</li>
+							<li class="has-subnav">
+								<a href="#">
+									<i class="fa fa-laptop fa-2x"></i>
+									<span class="nav-text">Stars Components</span>
+								</a>
+							</li>
+							<li class="has-subnav">
+								<a href="#">
+									<i class="fa fa-list fa-2x"></i>
+									<span class="nav-text">Forms</span>
+								</a>
+							</li>
+							<li class="has-subnav">
+								<a href="#">
+									<i class="fa fa-folder-open fa-2x"></i>
+									<span class="nav-text">Pages</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<i class="fa fa-bar-chart-o fa-2x"></i>
+									<span class="nav-text">Graphs and Statistics</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<i class="fa fa-font fa-2x"></i>
+									<span class="nav-text">Quotes</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<i class="fa fa-table fa-2x"></i>
+									<span class="nav-text">Tables</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<i class="fa fa-map-marker fa-2x"></i>
+									<span class="nav-text">Maps</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<i class="fa fa-info fa-2x"></i>
+									<span class="nav-text">Documentation</span>
+								</a>
+							</li>
+						</ul>
+
+						<ul class="logout">
+							<li>
+								<a href="#">
+									<i class="fa fa-power-off fa-2x"></i>
+									<span class="nav-text">Logout</span>
+								</a>
+							</li>
+						</ul>
+					</nav> */}
+					<div class="page-wrapper chiller-theme toggled">
+  <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
+    <i class="fas fa-bars"></i>
+  </a>
+  <nav id="sidebar" class="sidebar-wrapper">
+    <div class="sidebar-content">
+      <div class="sidebar-brand">
+        <a href="#">pro sidebar</a>
+        <div id="close-sidebar">
+          <i class="fas fa-times"></i>
+        </div>
+      </div>
+      <div class="sidebar-header">
+        <div class="user-pic">
+          <img class="img-responsive img-rounded" src="https://raw.githubusercontent.com/azouaoui-med/pro-sidebar-template/gh-pages/src/img/user.jpg" alt="User picture"/>
+        </div>
+        <div class="user-info">
+          <span class="user-name">Jhon
+            <strong>Smith</strong>
+          </span>
+          <span class="user-role">Administrator</span>
+          <span class="user-status">
+            <i class="fa fa-circle"></i>
+            <span>Online</span>
+          </span>
+        </div>
+      </div>
+      {/* <!-- sidebar-header  --> */}
+      <div class="sidebar-menu">
+        <ul>
+          <li class="header-menu">
+            <span>General</span>
+          </li>
+          <li class="sidebar-dropdown">
+            <a href="#">
+              <i class="fa fa-tachometer-alt"></i>
+              <span>Dashboard</span>
+              <span class="badge badge-pill badge-warning">New</span>
+            </a>
+            <div class="sidebar-submenu">
+              <ul>
+                <li>
+                  <a href="#">Dashboard 1
+                    <span class="badge badge-pill badge-success">Pro</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">Dashboard 2</a>
+                </li>
+                <li>
+                  <a href="#">Dashboard 3</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="sidebar-dropdown">
+            <a href="#">
+              <i class="fa fa-shopping-cart"></i>
+              <span>E-commerce</span>
+              <span class="badge badge-pill badge-danger">3</span>
+            </a>
+            <div class="sidebar-submenu">
+              <ul>
+                <li>
+                  <a href="#">Products
+
+                  </a>
+                </li>
+                <li>
+                  <a href="#">Orders</a>
+                </li>
+                <li>
+                  <a href="#">Credit cart</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="sidebar-dropdown">
+            <a href="#">
+              <i class="far fa-gem"></i>
+              <span>Components</span>
+            </a>
+            <div class="sidebar-submenu">
+              <ul>
+                <li>
+                  <a href="#">General</a>
+                </li>
+                <li>
+                  <a href="#">Panels</a>
+                </li>
+                <li>
+                  <a href="#">Tables</a>
+                </li>
+                <li>
+                  <a href="#">Icons</a>
+                </li>
+                <li>
+                  <a href="#">Forms</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="sidebar-dropdown">
+            <a href="#">
+              <i class="fa fa-chart-line"></i>
+              <span>Charts</span>
+            </a>
+            <div class="sidebar-submenu">
+              <ul>
+                <li>
+                  <a href="#">Pie chart</a>
+                </li>
+                <li>
+                  <a href="#">Line chart</a>
+                </li>
+                <li>
+                  <a href="#">Bar chart</a>
+                </li>
+                <li>
+                  <a href="#">Histogram</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="sidebar-dropdown">
+            <a href="#">
+              <i class="fa fa-globe"></i>
+              <span>Maps</span>
+            </a>
+            <div class="sidebar-submenu">
+              <ul>
+                <li>
+                  <a href="#">Google maps</a>
+                </li>
+                <li>
+                  <a href="#">Open street map</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="header-menu">
+            <span>Extra</span>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fa fa-book"></i>
+              <span>Documentation</span>
+              <span class="badge badge-pill badge-primary">Beta</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fa fa-calendar"></i>
+              <span>Calendar</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fa fa-folder"></i>
+              <span>Examples</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      {/* <!-- sidebar-menu  --> */}
+    </div>
+    {/* <!-- sidebar-content  --> */}
+    <div class="sidebar-footer">
+      <a href="#">
+        <i class="fa fa-bell"></i>
+        <span class="badge badge-pill badge-warning notification">3</span>
+      </a>
+      <a href="#">
+        <i class="fa fa-envelope"></i>
+        <span class="badge badge-pill badge-success notification">7</span>
+      </a>
+      <a href="#">
+        <i class="fa fa-cog"></i>
+        <span class="badge-sonar"></span>
+      </a>
+      <a href="#">
+        <i class="fa fa-power-off"></i>
+      </a>
+    </div>
+  </nav>
+  
+</div>
+{/* <!-- page-wrapper --> */}
 				</SideBarSec>
 				<Switch>
 					<Change>
