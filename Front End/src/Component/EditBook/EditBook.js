97b7d1b97f7783f9d8c3b60fd7cbbde0657a4524
sachintha00 <sachintha0603@gmail.com>
2021-09-29 10:42:08 +0530

    updated

---
+++ Front End/src/Component/EditBook/EditBook.js
@@ -0,0 +1,127 @@
+import React, { Component } from "react";
+
+export default class componentName extends Component {
+	constructor(props) {
+		super(props);
+	}
+	handleSubmit(event) {
+		event.preventDefault();
+
+		fetch("http://localhost:8082/book-service/book", {
+			method: "PUT",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				bookId: event.target.bookId.value,
+				bookName: event.target.bookName.value,
+				bookType: event.target.bookType.value,
+				medium: event.target.medium.value,
+				authorName: event.target.authorName.value,
+				isbn: event.target.isbn.value,
+				supplierId: event.target.supplierId.value,
+			}),
+		})
+			.then((res) => res.json())
+			.then(
+				(result) => {
+					alert("Success");
+				},
+				(error) => {
+					alert("kelawila");
+				}
+			);
+	}
+	render() {
+		return (
+			<Modal
+				{...this.props}
+				size="lg"
+				aria-labelledby="contained-modal-title-vcenter"
+				centered
+			>
+				<Modal.Header closeButton>
+					<Modal.Title id="contained-modal-title-vcenter">
+						Modal heading
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<h4>Centered Modal</h4>
+					<div className="Container">
+						<Form onSubmit={this.handleSubmit}>
+                        <Row className="mb-3">
+								<Form.Group as={Col} controlId="formGridSuppId">
+									<Form.Control as="select" name="supplierId">
+										<option defaultValue="Choose...">
+											SELECT SUPPLIER ID...
+										</option>
+										{this.state.supp.map((supp) => (
+											<option value={supp.supplierId}>{supp.name}</option>
+										))}
+									</Form.Control>
+								</Form.Group>
+							</Row>
+							<Row className="mb-3">
+                            <Form.Group as={Col} controlId="formGridBookId">
+									<Form.Control
+										type="text"
+										placeholder="Book ID"
+										name="bookId"
+									/>
+								</Form.Group>
+								<Form.Group as={Col} controlId="formGridName">
+									<Form.Control
+										type="text"
+										placeholder="Book Title"
+										name="bookName"
+									/>
+								</Form.Group>
+							</Row>
+							<Row className="mb-3">
+								<Form.Group as={Col} controlId="formGridType">
+									<Form.Control as="select" name="bookType">
+										<option value="">Select Book Type...</option>
+										<option value="1">1</option>
+										<option value="1">1</option>
+										<option value="1">1</option>
+									</Form.Control>
+								</Form.Group>
+								<Form.Group as={Col} controlId="formGridMedium">
+									<Form.Control as="select" name="medium">
+										<option value="">Select Medium...</option>
+										<option value="English">English</option>
+										<option value="Sinhala">Sinhala</option>
+										<option value="Tamil">Tamil</option>
+									</Form.Control>
+								</Form.Group>
+							</Row>
+							<Row className="mb-3">
+								<Form.Group as={Col} controlId="formGridAuthor">
+									<Form.Control
+										type="text"
+										placeholder="Author Name"
+										name="authorName"
+									/>
+								</Form.Group>
+								<Form.Group as={Col} controlId="formGridIsbn">
+									<Form.Control type="text" placeholder="ISBN" name="isbn" />
+								</Form.Group>
+							</Row>
+							<Row className="mb-3">
+								<Form.Group as={Col} controlId="formGridSubmit">
+									<Button variant="primary" type="submit">
+										Submit
+									</Button>
+								</Form.Group>
+							</Row>
+						</Form>
+					</div>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button onClick={this.props.onHide}>Close</Button>
+				</Modal.Footer>
+			</Modal>
+		);
+	}
+}
