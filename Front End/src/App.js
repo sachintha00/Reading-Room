94a1a3e8e59edb2a1df9c8656ebe0fe96f139145
sachintha00 <sachintha0603@gmail.com>
2021-10-01 17:22:36 +0530

    updated

--- Front End/src/App.js
+++ Front End/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LoginPage from "./Component/Login/LoginPage";
-import Dashboard from "./Component/DashBoard/DashBoard";
+import Dashboard from "./Component/DashBoard/dashboard";
 
 function App() {
 	return (
